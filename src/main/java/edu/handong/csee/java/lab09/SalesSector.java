cd5e4ab9a4d5c33a20201dbd4795b41af3d97198
jungyeun <yjyj989812@naver.com>
2018-04-11 00:32:11 +0900

    realy clear add the coment

--- src/main/java/edu/handong/csee/java/lab09/SalesSector.java
+++ src/main/java/edu/handong/csee/java/lab09/SalesSector.java
@@ -1,22 +1,22 @@
 package edu.handong.csee.java.lab09;
 
 public class SalesSector {
-	private String name ;
-	private double sales ;
+	private String name ;		// set the String variable as name
+	private double sales ;		// set the double variable as sales
 	
 	public void setName(String name) {
-		this.name = name ;
+		this.name = name ;		// input the name to SalesSector.name
 	}
 	
 	public void setSales(double sales) {
-		this.sales = sales ;
+		this.sales = sales ;	// input the sales to SalesSector.sales
 	}
 	
 	public String getName() {
-		return this.name ;
+		return this.name ;		// return the SalesSector.name value
 	}
 	
 	public double getSales() {
-		return this.sales ;
+		return this.sales ;		// return the SalesSector.sales value
 	}
 }
